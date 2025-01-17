4fce9f6c3d2972f88229109ec0bd5efc4c52fb2a
Divyyanshu <divvyyanshuutailor@gmail.com>
2025-01-17 22:46:11 +0530

    client add

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center'>
+    <div className='text-red-500 font-bold text-4xl p-10'>
+      hello
+    </div>
+    </div>
+  )
+}
+
+export default App
